5eb49d8f95474e63699df964a14211b78d8fc152
Kuirak <newdrako@gmx.net>
2011-04-22 00:27:48 +0200

    generateBlock with GL_TRIANGLE_STRIP

--- src/com/drako/tdgdx/TdGdx.java
+++ src/com/drako/tdgdx/TdGdx.java
@@ -1,13 +1,24 @@
 package com.drako.tdgdx;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.drako.tdgdx.helper.MeshObject;
 
 public class TdGdx implements ApplicationListener {
-
+	private PerspectiveCamera camera;
+	private MeshObject meshObj;
+	
+	float rotate;
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
-		
+		Gdx.app.log("Create", "Start Init");
+		camera = new PerspectiveCamera(67, 500, 300);
+		meshObj = new MeshObject(0, 0, 10, 10,10);
+	
+		rotate =0;
+		Gdx.app.log("Create", "Finished Init");
 	}
 
 	@Override
@@ -18,7 +29,21 @@ public class TdGdx implements ApplicationListener {
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
+		GL10 gl = Gdx.graphics.getGL10();
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		
+		camera.update();
+		camera.apply(gl);
+		
+		gl.glPushMatrix();
+		gl.glTranslatef(meshObj.getPosition().x,meshObj.getPosition().y, -40);
+		gl.glRotatef(rotate, 0, 1, 0);
+		meshObj.getMesh().render(GL10.GL_TRIANGLE_STRIP);
+		gl.glPopMatrix();
+		
+		rotate++;
+		
 		
 	}
 
--- src/com/drako/tdgdx/TdGdxDesktopStarter.java
+++ src/com/drako/tdgdx/TdGdxDesktopStarter.java
@@ -5,5 +5,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 public class TdGdxDesktopStarter {
 public static void main(String[] args) {
 	new LwjglApplication(new TdGdx(), "Tower Defense Android - GDX", 500, 300, false);
+	
 }
 }
